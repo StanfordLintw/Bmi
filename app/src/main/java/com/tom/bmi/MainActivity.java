5cf353f62a0fde7505406562e65e8d14984ebc3a
Hank Tom <hanktom@gmail.com>
2017-11-21 12:04:14 +0800

    ResultActivity & Lifecycle

--- app/src/main/java/com/tom/bmi/MainActivity.java
+++ app/src/main/java/com/tom/bmi/MainActivity.java
@@ -1,5 +1,7 @@
 package com.tom.bmi;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,12 +13,16 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     String s = new String("abc");
+    private EditText edWeight;
+    private EditText edHeight;
+    private Button bHelp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button bHelp = (Button) findViewById(R.id.b_help);
+        Log.d("MainActivity", "onCreate" );
+        findViews();
         bHelp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,21 +33,63 @@ public class MainActivity extends AppCompatActivity {
                         .show();
             }
         });
-
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity", "onStart" );
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActivity", "onStop" );
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MainActivity", "onDestroy" );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MainActivity", "onPause" );
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MainActivity", "onResume" );
+    }
+
+    private void findViews() {
+        edWeight = (EditText) findViewById(R.id.ed_weight);
+        edHeight = (EditText) findViewById(R.id.ed_height);
+        bHelp = (Button) findViewById(R.id.b_help);
+    }
+
+
     public void bmi(View view){
 //        System.out.println("what???");
         Log.d("MainActivity", "testing bmi method");
-        EditText edWeight = (EditText) findViewById(R.id.ed_weight);
-        EditText edHeight = (EditText) findViewById(R.id.ed_height);
+
         float weight = Float.parseFloat(edWeight.getText().toString());
         float height = Float.parseFloat(edHeight.getText().toString());
         float bmi = weight / (height * height);
-        new AlertDialog.Builder(this)
+
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra(getString(R.string.extra_bmi), bmi);
+        startActivity(intent);
+
+
+        /*new AlertDialog.Builder(this)
                 .setMessage(getString(R.string.your_bmi_is) + bmi)
                 .setTitle(R.string.bmi_title)
                 .setPositiveButton(R.string.ok, null)
-                .show();
+                .show();*/
 
        /* Log.d("MainActivity", "You BMI is : " + bmi);
         Toast.makeText(this,
